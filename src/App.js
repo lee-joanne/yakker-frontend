b20f69ea939b0050c0e47780eaee684682cb2278
Joanne <lee.joanne54@gmail.com>
2022-11-24 19:55:55 +0000

    Create LoginForm.js file and added in strucutral design code and onChange and onSubmit functions to allow users to log in. Add links to signup form in login link and login form link in sign up link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import { Routes, Route } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForm"
+import LoginForm from "./pages/auth/LoginForm"
 import Footer from './footer';
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <Container className={styles.Main}>
         <Routes>
           <Route exact path="/" render={() => <h1>Home</h1>} />
-          <Route exact path="/signin" render={() => <h1>Login</h1>} />
+          <Route exact path="/login" element={<LoginForm />} />
           <Route exact path="/signup" element={<SignUpForm />} />
           <Route render={() => <h1>Sorry! Not found.</h1>} />
         </Routes>
--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -6,22 +6,21 @@ import { Form, Button, Image, Row, Container, Col, Alert } from "react-bootstrap
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 
-const SignUpForm = () => {
-    const [signUpData, setSignUpData] = useState({
+const LoginForm = () => {
+    const [loginData, setloginData] = useState({
         username: "",
-        password1: "",
-        password2: "",
+        password: "",
     });
 
     const [errors, setErrors] = useState({});
 
-    const { username, password1, password2 } = signUpData;
+    const { username, password } = loginData;
 
     const navigate = useNavigate();
 
     const handleChange = (event) => {
-        setSignUpData({
-            ...signUpData,
+        setloginData({
+            ...loginData,
             [event.target.name]: event.target.value,
         })
     }
@@ -29,8 +28,8 @@ const SignUpForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            await axios.post('/dj-rest-auth/registration/', signUpData)
-            navigate('/login')
+            await axios.post('/dj-rest-auth/login/', loginData)
+            navigate('/')
         } catch (err) {
             setErrors(err.response?.data)
         }
@@ -44,7 +43,7 @@ const SignUpForm = () => {
                         className={styles.LogoImage}
                         src={logoImage}
                     />
-                    <h1 className="mb-3">Sign Up</h1>
+                    <h1 className="mb-3">Login</h1>
                     <Row className="justify-content-center">
 
                         <Form onSubmit={handleSubmit}>
@@ -55,23 +54,16 @@ const SignUpForm = () => {
                             {errors.username?.map((message, idx) =>
                                 <Alert variant="warning" key={idx}>{message}</Alert>)}
 
-                            <Form.Group controlId="password1" className="text-left mb-2">
+                            <Form.Group controlId="password" className="text-left mb-2">
                                 <Form.Label className="d-none">Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" name="password1" value={password1} onChange={handleChange} />
+                                <Form.Control type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
                             </Form.Group>
                             {errors.password1?.map((message, idx) =>
                                 <Alert variant="warning" key={idx}>{message}</Alert>)}
 
-                            <Form.Group controlId="password2" className="text-left">
-                                <Form.Label className="d-none">Password</Form.Label>
-                                <Form.Control type="password" placeholder="Confirm password" name="password2" value={password2} onChange={handleChange} />
-                            </Form.Group>
-                            {errors.password2?.map((message, idx) =>
-                                <Alert variant="warning" key={idx}>{message}</Alert>)}
-
                             <br />
                             <Button className={btnStyles.btn} variant="primary" type="submit">
-                                Sign me up
+                                Login!
                             </Button>
                             {errors.non_field_errors?.map((message, idx) => (
                                 <Alert key={idx} variant="warning" className="mt-3">{message}</Alert>
@@ -79,8 +71,8 @@ const SignUpForm = () => {
                         </Form>
                     </Row>
                     <Row className="mt-4 justify-content-center">
-                        <p>Already have an account? <span><Link className={styles.Link} to="/login">
-                            Login
+                        <p>Don't have an account yet? <span><Link className={styles.Link} to="/signup">
+                            Sign up
                         </Link></span> instead!</p>
                     </Row>
                 </Col>
@@ -89,4 +81,4 @@ const SignUpForm = () => {
     )
 }
 
-export default SignUpForm
+export default LoginForm
