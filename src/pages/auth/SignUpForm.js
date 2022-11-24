8046de56a6b6a94bdc3a56f1a4e3c43e4a827bac
Joanne <lee.joanne54@gmail.com>
2022-11-24 00:23:43 +0000

    Successfully add alert messages for any errors captured, fix navigation when created account is successful to navigate user to login page after creation

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -9,11 +9,13 @@ import { useNavigate } from 'react-router-dom';
 const SignUpForm = () => {
     const [signUpData, setSignUpData] = useState({
         username: "",
-        password: "",
+        password1: "",
         password2: "",
     });
 
-    const { username, password, password2 } = signUpData;
+    const [errors, setErrors] = useState({});
+
+    const { username, password1, password2 } = signUpData;
 
     const navigate = useNavigate();
 
@@ -28,9 +30,9 @@ const SignUpForm = () => {
         event.preventDefault();
         try {
             await axios.post('/dj-rest-auth/registration/', signUpData)
-            navigate.push('/login')
+            navigate('/login')
         } catch (err) {
-            console.log(err)
+            setErrors(err.response?.data)
         }
     };
 
@@ -50,22 +52,30 @@ const SignUpForm = () => {
                                 <Form.Label className="d-none">Username</Form.Label>
                                 <Form.Control type="text" placeholder="Username" name="username" value={username} onChange={handleChange} />
                             </Form.Group>
+                            {errors.username?.map((message, idx) =>
+                                <Alert variant="warning" key={idx}>{message}</Alert>)}
 
-
-                            <Form.Group controlId="password" className="text-left mb-2">
+                            <Form.Group controlId="password1" className="text-left mb-2">
                                 <Form.Label className="d-none">Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
+                                <Form.Control type="password" placeholder="Password" name="password1" value={password1} onChange={handleChange} />
                             </Form.Group>
+                            {errors.password1?.map((message, idx) =>
+                                <Alert variant="warning" key={idx}>{message}</Alert>)}
 
                             <Form.Group controlId="password2" className="text-left">
                                 <Form.Label className="d-none">Password</Form.Label>
                                 <Form.Control type="password" placeholder="Confirm password" name="password2" value={password2} onChange={handleChange} />
                             </Form.Group>
+                            {errors.password2?.map((message, idx) =>
+                                <Alert variant="warning" key={idx}>{message}</Alert>)}
 
                             <br />
                             <Button className={btnStyles.btn} variant="primary" type="submit">
                                 Sign me up
                             </Button>
+                            {errors.non_field_errors?.map((message, idx) => (
+                                <Alert key={idx} variant="warning" className="mt-3">{message}</Alert>
+                            ))}
                         </Form>
                     </Row>
                     <Row className="mt-4 justify-content-center">
