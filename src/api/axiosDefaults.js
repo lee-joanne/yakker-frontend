66d25df8810f0e1fec28a7496a84a81fa881ec60
Joanne <lee.joanne54@gmail.com>
2022-11-25 18:26:58 +0000

    Add axios access tokens to CurrentUserContext.js file for res and req interceptors to allow user to stay logged in longer and check user's current logged in status

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,10 @@
+// Code taken from CI's Moments project.
+
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://yakker-backend.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create(); // Other requests
+export const axiosRes = axios.create(); //Get requests
