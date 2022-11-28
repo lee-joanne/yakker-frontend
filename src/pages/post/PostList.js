62fce89a9ec762831d1b0704212054c709e13bec
Joanne <lee.joanne54@gmail.com>
2022-11-28 18:57:42 +0000

    Add boilerplate code for PostList.js file, adjust naming of the file for PostList.module.css

--- src/pages/post/PostList.js
+++ src/pages/post/PostList.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Container } from "react-bootstrap";
+import styles from "./../../styles/PostList.module.css"
 
 const PostList = () => {
     return (
-        <div>ExplorePage</div>
+        <Row>
+            <Col className={styles.HeaderText} lg={8}>
+                <p>Popular profiles</p>
+                <Container>
+                    Comments
+                </Container>
+            </Col>
+            <Col className={styles.HeaderText} lg={3}>
+                <p>
+                    Popular profiles for desktop
+                </p>
+            </Col>
+        </Row>
     )
 }
 
